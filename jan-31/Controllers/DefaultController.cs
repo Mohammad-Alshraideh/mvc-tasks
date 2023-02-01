d2ee670edd64cadfb3e8e42681b637ee0ca38be0
unknown <mohammad.w.alshraideh@gmail.com>
2023-02-01 09:48:16 +0300

    mvc-task-1

---
+++ jan-31/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jan_31.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            return "<a href='img/bg_1.jpg' download><img src='img/bg_1.jpg' style='width:200px'/></a>";
+
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult download()
+        {
+            var filePath = Server.MapPath("~/img/bg_1.jpg");
+            return File(filePath, "jpg", "el-bg_1.jpg");
+             
+            
+        }
+        public string About()
+        {
+
+            return "<h1>About us <h1/>";
+
+
+        }
+        public string Contact()
+        {
+
+            return "<h1>Contact us <h1/>";
+
+
+        }
+    }
+}
---
+++ jan-31/Controllers/ShraidehController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jan_31.Controllers
+{
+    public class ShraidehController : Controller
+    {
+        // GET: Shraideh
+        public string Index()
+        {
+            return $"<img '/>";
+        }
+        public FileResult path()
+        {
+            var path = Server.MapPath("~/img/bg_1.jpg");
+            return File(path, "jpg" );
+        }
+        public string str()
+        {
+            return "hello";
+        }
+       public ContentResult cr()
+        {
+            int x = 1;
+            return Content(x.ToString());
+        }
+        public ActionResult ar()
+        {
+            return Content("not hello");
+        }
+    }
+}
