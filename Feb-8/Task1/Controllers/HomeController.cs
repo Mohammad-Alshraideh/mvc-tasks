d5e0f7a2c8f2024b9d1dfeea8d6559bbc191349a
unknown <mohammad.w.alshraideh@gmail.com>
2023-02-08 15:27:17 +0300

    feb-8

---
+++ Feb-8/Task1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        feb7Entities2 db = new feb7Entities2();
+        public ActionResult Index()
+        {
+            return View(db.Comments.ToList());
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
